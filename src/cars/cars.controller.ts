a3c90d2579852bbec27c71678d22b60b344afcae
HeiderHDev <heiderhrnndz@gmail.com>
2023-12-20 23:30:08 -0500

    Primer commit del curso de nestjs de fernando herrera

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+  private cars = ['Toyota', 'Honda', 'Jeep']
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+    
+}
